5d374ac12676b205d60889c9d0f8669e09c0f120
bjyberg <bjyberg1@gmail.com>
2022-09-13 23:39:39 +0200

    Fixing Errors

--- DataProcessing.R
+++ DataProcessing.R
@@ -5,30 +5,34 @@ source('UAV_indices.R')
 ####Setup Variables####
 Site <- 1 #list site number for saving outputs if performing training and analysis on multiple sites
 #import data paths
-AOI <- vect('/data/shp/S1Area.shp') #List as NA if not cropping sites 
-Micasense.Full <-rast('/data/Fullsite/S1_mica.tif', lyrs = c(1,2,3,4,5)) #MicaSense Imagery
-DJI.full <-rast('data/Fullsite/S1_dji.tif', lyrs = c(1,2,3)) #DJI RGB imagery
-dsm.mica.full <- rast('/data/Fullsite/S1_mica_DSM.tif') #MicaSense DSM
-dsm.dji.full <- rast('/data/Fullsite/S1_dji_DSM.tif') #DJI DSM
-twi.mica.full <- rast('/data/Fullsite/S1_Mica_TWI.tif') #MicaSense Topographic Wetness Index (TWI) created using SAGA in QGIS
-twi.dji.full <- rast('/home/bjyberg/Peatland/Ardgowan_Data/TWI/DJI_TWI.tif') #DJI TWI
+#If listed as "NOT REQUIRED" fill using NA or don't create object
+AOI <- vect('/home/bjyberg/Peatland/Ardgowan_Data/ShapeFiles/SmallerTestArea.shp') # AOI if using a subsection of a site - NOT REQUIRED
+Micasense.Full <-rast('/home/bjyberg/Peatland/Ardgowan_Data/Site1/S1_mica.tif', lyrs = c(1,2,3,4,5)) #MicaSense Imagery - REQUIRED
+DJI.full <-rast('/home/bjyberg/Peatland/Ardgowan_Data/Site1/S1_dji.tif', lyrs = c(1,2,3)) #DJI RGB imagery -REQUIRED
+dsm.mica.full <- rast('/home/bjyberg/Peatland/Ardgowan_Data/Site1/S1_DSM_mica.tif') #MicaSense DSM - NOT REQUIRED
+dsm.dji.full <- NA #DJI DSM - NOT REQUIRED
+##twi.mica.full <- rast('/data/Fullsite/S1_Mica_TWI.tif') #MicaSense Topographic Wetness Index (TWI) created using SAGA in QGIS - NOT REQUIRED
+twi.dji.full <- rast('/home/bjyberg/Peatland/Ardgowan_Data/TWI/DJI_TWI.tif') #DJI TWI - NOT REQUIRED
 #Set path for processed data outputs
-output_folder <- ('/data/prepared')
+output_folder <- ('~/test')
 
 startTime <- Sys.time() #Start Clock
 
 ####Prep MicaSense####
-names(Micasense.Full) <- c('blue','green', 'red', 'Nir', 'Redge')e
-
+names(Micasense.Full) <- c('blue','green', 'red', 'Nir', 'Redge')
 #Micasense.full <- classify(Micasense.full, cbind(65535, NA)) #Not needed if cropping -- fixing out of extent NA values incorrectly assigned as 65535
 
-if (is.na(AOI) != TRUE) {
- Micasense <- Micasense.Full
- print('No Crop Performed')
+if (exists("AOI")) {
+  if (is.na(AOI) == TRUE) {
+  Micasense <- Micasense.Full
+  print('No Crop Performed')
   } else {
     Micasense <- terra::crop(Micasense.Full, AOI)#crop to area of interest if provided
     print('Crop Performed')
-    }
+  }
+} else {
+  AOI <- NA
+}
 
 MicaRef <- Mica.reflectance(Micasense, 1:5) #calculate normalized reflectance for Micasense
 
@@ -38,40 +42,46 @@ GNDVI.Mica <- GNDVI(MicaRef) #function defaults to required bands if named corre
 NDRE.Mica <- NDRE(MicaRef)
 EVI.Mica <- EVI(MicaRef)
 MTVI2.Mica <- MTVI2(MicaRef)
-REsimple.Mica <- MTVI2(MicaRef)
-coreRtvi.Mica <- MTVI2(MicaRef)
-MCARI.Mica <- MCARI(MicaRef)
+REsimple.Mica <- RedgeSimple(MicaRef)
+coreRtvi.Mica <- coreRtvi(MicaRef)
+MCARI2.Mica <- MCARI2(MicaRef)
 PCA.Mica <- rasterPCA(MicaRef, nComp = 2) #adjust nComp as needed
 PCA1 <- rast(PCA.Mica$map$PC1)
 PCA2 <- rast(PCA.Mica$map$PC2)
 
 ##stack Indicies
 MicaIndices <- c(NDVI.Mica, GNDVI.Mica, NDRE.Mica, EVI.Mica, MTVI2.Mica, 
-                    REsimple.Mica, coreRtvi.Mica, MCARI.Mica, PCA1, PCA2)
+                    REsimple.Mica, coreRtvi.Mica, MCARI2.Mica, PCA1, PCA2)
 
 names(MicaIndices) <- c('NDVI.Mica', 'GNDVI.Mica', 'NDRE.Mica', 'EVI.Mica', 'MTVI2.Mica', 
-                           'REsimple.Mica', 'coreRtvi.Mica', 'MCARI.Mica', 'PCA1', 'PCA2')
+                           'REsimple.Mica', 'coreRtvi.Mica', 'MCARI2.Mica', 'PCA1', 'PCA2')
 
-writeRaster(micaIndices, paste0(output_folder, '/', Site, 'Mica_Indicies.tif'))
+writeRaster(MicaIndices, paste0(output_folder, '/', Site, 'Mica_Indicies.tif'))
 
 ###Prep Micasense Terrain
-if (exists("dsm.mica.full")) {
-  if (is.na(AOI) != TRUE) {
-   dsm.mica <- dsm.mica.full
-   print("No Cropping Performed")
+if (!exists("dsm.mica.full")) {
+  dsm.mica.full <- NA
+}
+
+if (is.na(dsm.mica.full) == FALSE) {
+  if (is.na(AOI) == FALSE) {
+  dsm.mica <- dsm.mica.full
+  print("No Cropping Performed")
   } else {
-    dsm.mica <- terra::crop(dsm.mica.full, AOI) #crop to area of interest if provided
+  dsm.mica <- terra::crop(dsm.mica.full, AOI) #crop to area of interest if provided
   }
 } else {
-  print("Warning No Terrain Data Provided for Micasense")
+    print("Warning: No Terrain Data Provided for Micasense")
+}
+  
+if (is.na(dsm.mica.full) == FALSE) {
+    terrain.mica <- terrain(dsm.mica, v = c('slope', 'TPI','roughness'), unit = 'degrees')
   }
-
-if (exists("dsm.mica.full")) {
-  terrain.mica <- terrain(dsm.mica, v = c('slope', 'TPI','roughness'), unit = 'degrees')
 }
 
 if (exists("twi.mica.full")) {
-  if (is.na(AOI) != TRUE) {
+  if (is.na(twi.mica.full) == FALSE)
+  if (is.na(AOI) == FALSE) {
   twi.mica <- twi.mica.full
   } else {
     twi.mica <- crop(twi.mica.full, AOI)
@@ -80,24 +90,31 @@ if (exists("twi.mica.full")) {
 
 ## Stack Mica terrain
 if (exists("twi.mica.full")) {
-  Mica.TRindices <- c(dsm.mica, terrain.mica, twi.mica)
-  names(Mica.TRindices) <- c('DSM', 'slope', 'tpi', 'roughness', 'twi')
-} else if (exists("dsm.mica.full")) {
-  Mica.TRindices <- c(dsm.mica, terrain.mica,)
-  names(Mica.TRindices) <- c('DSM', 'slope', 'tpi', 'roughness')
+  if (is.na(twi.mica.full) == FALSE) {
+    Mica.TRindices <- c(dsm.mica, terrain.mica, twi.mica)
+    names(Mica.TRindices) <- c('DSM', 'slope', 'tpi', 'roughness', 'twi')
+  } else if (exists("dsm.mica.full")) {
+    Mica.TRindices <- c(dsm.mica, terrain.mica,)
+    names(Mica.TRindices) <- c('DSM', 'slope', 'tpi', 'roughness')
+  }
 }
 
 if (exists("Mica.TRindices")) {
-writeRaster(Mica.TRindices, paste0(output_folder, '/', Site, 'MicaTRIndices.tif'))
+  if (is.na(twi.mica.full) == FALSE) {
+    writeRaster(Mica.TRindices, paste0(output_folder, '/', Site, 'MicaTRIndices.tif'))
+  }
 }
 
 ####Prep DJI####
 names(RGB.full) <- c('red', 'green', 'blue')
 #RGB.full <- classify(RGB.full, cbind(250, NA)) #Not needed if cropping -- fixing out of extent NA values incorrectly assigned as 250
-if (is.na(AOI) != TRUE) {
-  DJI.rgb <- RGB.full
-} else {
-  DJI.rgb <- terra::crop(RGB.full, AOI) #crop to area of interest if provided
+
+if (exists(twi.mica.full))
+  { if (is.na(AOI) != TRUE) {
+    DJI.rgb <- RGB.full
+  } else {
+    DJI.rgb <- terra::crop(RGB.full, AOI) #crop to area of interest if provided 
+  }
 }
 
 ##create indices
