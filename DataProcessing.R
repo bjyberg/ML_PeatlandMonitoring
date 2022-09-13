2859fc260d70ba006c0c325a0906bf629ab17639
bjyberg <bjyberg1@gmail.com>
2022-09-13 21:52:28 +0200

    Adding if statements

--- DataProcessing.R
+++ DataProcessing.R
@@ -2,25 +2,26 @@ library(terra)
 library(RStoolbox)
 source('UAV_indices.R')
 
-####Setup Variables###
+####Setup Variables####
 Site <- 1 #list site number for saving outputs if performing training and analysis on multiple sites
-#import data
+#import data paths
 AOI <- vect('/data/shp/S1Area.shp') #List as NA if not cropping sites 
 Micasense.Full <-rast('/data/Fullsite/S1_mica.tif', lyrs = c(1,2,3,4,5)) #MicaSense Imagery
 DJI.full <-rast('data/Fullsite/S1_dji.tif', lyrs = c(1,2,3)) #DJI RGB imagery
 dsm.mica.full <- rast('/data/Fullsite/S1_mica_DSM.tif') #MicaSense DSM
 dsm.dji.full <- rast('/data/Fullsite/S1_dji_DSM.tif') #DJI DSM
 twi.mica.full <- rast('/data/Fullsite/S1_Mica_TWI.tif') #MicaSense Topographic Wetness Index (TWI) created using SAGA in QGIS
-twi.dji.full <- rast('TWI/S1_DJI_TWI.tif') #DJI TWI
+twi.dji.full <- rast('/home/bjyberg/Peatland/Ardgowan_Data/TWI/DJI_TWI.tif') #DJI TWI
 #Set path for processed data outputs
 output_folder <- ('/data/prepared')
 
 startTime <- Sys.time() #Start Clock
 
-##Prep MicaSense###
-names(Micasense.Full) <- c('blue','green', 'red', 'Nir', 'Redge')
+####Prep MicaSense####
+names(Micasense.Full) <- c('blue','green', 'red', 'Nir', 'Redge')e
 
-#Site1_Micasense <- classify(Site1_Micasense, cbind(65535, NA)) #Not needed if cropping -- fixing out of extent NA values incorrectly assigned as 65535
+#Micasense.full <- classify(Micasense.full, cbind(65535, NA)) #Not needed if cropping -- fixing out of extent NA values incorrectly assigned as 65535
+
 if (is.na(AOI) != TRUE) {
  Micasense <- Micasense.Full
  print('No Crop Performed')
@@ -54,26 +55,43 @@ names(MicaIndices) <- c('NDVI.Mica', 'GNDVI.Mica', 'NDRE.Mica', 'EVI.Mica', 'MTV
 writeRaster(micaIndices, paste0(output_folder, '/', Site, 'Mica_Indicies.tif'))
 
 ###Prep Micasense Terrain
-if (is.na(AOI) != TRUE) {
-  dsm.mica <- dsm.mica.full
+if (exists("dsm.mica.full")) {
+  if (is.na(AOI) != TRUE) {
+   dsm.mica <- dsm.mica.full
+   print("No Cropping Performed")
+  } else {
+    dsm.mica <- terra::crop(dsm.mica.full, AOI) #crop to area of interest if provided
+  }
 } else {
-  dsm.mica <- terra::crop(dsm.mica.full, AOI) #crop to area of interest if provided
+  print("Warning No Terrain Data Provided for Micasense")
+  }
+
+if (exists("dsm.mica.full")) {
+  terrain.mica <- terrain(dsm.mica, v = c('slope', 'TPI','roughness'), unit = 'degrees')
 }
 
-terrain.mica <- terrain(dsm.mica, v = c('slope', 'TPI','roughness'), unit = 'degrees')
-if (is.na(AOI) != TRUE) {
+if (exists("twi.mica.full")) {
+  if (is.na(AOI) != TRUE) {
   twi.mica <- twi.mica.full
-} else {
-  twi.mica <- crop(twi.mica.full, AOI)
+  } else {
+    twi.mica <- crop(twi.mica.full, AOI)
+  }
 }
 
 ## Stack Mica terrain
-Mica.TRindices <- c(dsm.mica, terrain.mica, twi.mica)
-names(S1.Mica.TRindices) <- c('DSM', 'slope', 'tpi', 'roughness', 'twi')
+if (exists("twi.mica.full")) {
+  Mica.TRindices <- c(dsm.mica, terrain.mica, twi.mica)
+  names(Mica.TRindices) <- c('DSM', 'slope', 'tpi', 'roughness', 'twi')
+} else if (exists("dsm.mica.full")) {
+  Mica.TRindices <- c(dsm.mica, terrain.mica,)
+  names(Mica.TRindices) <- c('DSM', 'slope', 'tpi', 'roughness')
+}
 
-writeRaster(S1.Mica.TRindices, paste0(output_folder, '/', Site, 'MicaTRIndices.tif'))
+if (exists("Mica.TRindices")) {
+writeRaster(Mica.TRindices, paste0(output_folder, '/', Site, 'MicaTRIndices.tif'))
+}
 
-###Prep DJI###
+####Prep DJI####
 names(RGB.full) <- c('red', 'green', 'blue')
 #RGB.full <- classify(RGB.full, cbind(250, NA)) #Not needed if cropping -- fixing out of extent NA values incorrectly assigned as 250
 if (is.na(AOI) != TRUE) {
@@ -94,22 +112,44 @@ names(DJI.indices) <- c('GLI', 'SBrigntness', 'Brightness_avg')
 writeRaster(DJI.indices, paste0(output_folder, '/', Site, 'DJI_indicies.tif'))
 
 ##Prep DJI terrain
-dsm.dji.full <- rast('FullSite/S1_dji_DSM.tif')
-dsm.dji <- terra::crop(dsm.dji.full, AOI)
-
-terrain.dji <- terra::terrain(dsm.dji, v = c('slope', 'TPI','roughness'), unit = 'degrees')
-if (is.na(AOI) != TRUE) {
-  twi.dji <- twi.dji.full
+if (exists("dsm.dji.full")) {
+  if (is.na(AOI) != TRUE) {
+    dsm.dji <- dsm.dji.full
+    print("No Cropping Performed")
+  } else {
+    dsm.dji <- terra::crop(dsm.dji.full, AOI) #crop to area of interest if provided
+  }
 } else {
-  twi.dji <- crop(twi.dji.full, AOI)
+  print("Warning No Terrain Data Provided for DJI")
 }
 
-##Stack dji terrain
-DJI.TRindices <- c(Sdsm.dji, terrain.dji, twi.dji)
-names(DJI.TRindices) <- c('dsm', 'slope', 'tpi', 'roughness', 'twi')
-writeRaster(DJI.TRindices, paste0(output_folder, '/', Site, 'DJI_TRindices.tif'))
+if (exists("dsm.dji.full")) {
+  terrain.dji <- terrain(dsm.dji, v = c('slope', 'TPI','roughness'), unit = 'degrees')
+}
 
-#Print total processing time
+if (exists("twi.dji.full")) {
+  if (is.na(AOI) != TRUE) {
+    twi.dji <- twi.dji.full
+  } else {
+    twi.dji <- crop(twi.dji.full, AOI)
+  }
+}
+
+## Stack DJI terrain
+if (exists("twi.dji.full")) {
+  DJI.TRindices <- c(Sdsm.dji, terrain.dji, twi.dji)
+  names(DJI.TRindices) <- c('dsm', 'slope', 'tpi', 'roughness', 'twi')
+} else if (exists("dsm.dji.full")) {
+  DJI.TRindices <- c(Sdsm.dji, terrain.dji)
+  names(DJI.TRindices) <- c('dsm', 'slope', 'tpi', 'roughness')
+}
+
+if (exists("DJI.TRindices")) {
+  writeRaster(DJI.TRindices, paste0(output_folder, '/', Site, 'DJI_TRindices.tif'))
+}
+
+
+####Print total processing time####
 timeDiff <- Sys.time() - startTime
 cat('Processing time:', format(timeDiff), '\n')
 
