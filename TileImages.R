d27974a7eaf6991035110cff2bd11a67e957e9ec
bjyberg <bjyberg1@gmail.com>
2022-11-30 18:09:50 +0000

    clean and for loop

--- TileImages.R
+++ TileImages.R
@@ -1,5 +1,4 @@
 #function for augmentation - copy the magic + some
-library(stars)
 library(sf)
 library(terra)
 library(doParallel)
@@ -15,64 +14,10 @@ rast_bands <- c(1:5) #list of bands to read in - e.g., c(1,2,3,5,10) or 1:4
 tile_dimensions <- c(64, 64)
 length(rast_bands)
 print(rast_bands)
-####terra functions test runs ####
-labs <- rasterize_labels(labels_vect_path, image_path, Field='Num_class')
-
-xyres <- res(rast)
-round(xyres[1], 3) == round(xyres[2], 3) #is it square?
-n_pixels <- 32 #number of pixels per tile#
-cell_size <- round((n_pixels*xyres), 3) #size (m) per patch
-cell_grid <- st_make_grid(rast, cellsize = cell_size)
-patch_cells <- lengths(st_within(cell_grid, st_as_sfc(st_bbox(rast)))) >0
-patch_grid <- cell_grid[patch_cells]
-plot(rast$MicaClip_1)
-plot(patch_grid, add = TRUE)
-rast <- c(labs, rast)
-
-for (i in 1:length(patch_grid)) { #add option for parallell and put in function
-  tile_i <- rast[[-'labels']] %>% 
-    crop(patch_grid[i])
-  dir.create(paste0(output_patches_dir, '/', 'patched_images'))
-  image.patch.dir <- paste0(output_patches_dir, '/', 'patched_images')
-  writeRaster(tile_i, paste0(image.patch.dir, '/','image_patch_', i, '.tif'))
-  
-  label_tile_i <- rast[['labels']] %>%
-    crop(patch_grid[i])
-  dir.create(paste0(output_patches_dir, '/', 'patched_labs'))
-  writeRaster(label_tile_i, paste0(label.patch.dir,
-                                   '/','label_patch_', i, '.tif'))
-}
-
-label_tile_I <- rast['Num_class'] %>% #[must be title or layer, make ground mask always 1]
-  crop(patch_grid[50])
-
-plot(label_tile_I)
-
-plot(crop(rast[[1]],patch_grid[1]))
-plot(patch_grid[1], add = TRUE)
-plot(patch_grid[c(1,2,3,4,5)])
-
-
-
-
-
-
-
-
-
-####Star test runs####
-labs <- read_stars(labels_path)
-im <- read_stars(image_path)
-st_dimensions(im)
-st_dimensions(labs)
-warped <- st_warp(labs, im)
-cropped <- st_crop(im, labs)
-st_crs(im)[1]
-st_crs(labs)[2]
-
 
 #Terra Functions----
 rasterize_labels <- function(labels, field, image_raster, output_path){
+  start.time <- Sys.time()
   if (crs(labels) != crs(image_raster)) {
     print(paste('Image transformation required for labels from',
                 as.character(st_crs(labels)[1]), 'to', 
@@ -83,10 +28,56 @@ rasterize_labels <- function(labels, field, image_raster, output_path){
   if (!missing(output_path)) {
       writeRaster(labs_raster, paste0(output_path,'Label_Raster', '.tif'))
   }
-  return(labs_raster)
+  stop.time <- Sys.time()
+  overall_time <- stop.time - start.time
+  
+  return(list(overall_time, labs_raster))
 }
 
 dl_training_tile <- function(image_raster, label_raster, n_pixels, output_path,
+                             site_name) {
+  start.time <- Sys.time()
+  cropped.labels <- crop(label_raster, image_raster)
+  rast.stack <- c(cropped.labels, image_raster)
+  xyres <- res(rast.stack)
+  round(xyres[1], 3) == round(xyres[2], 3) #is it square? ~~~~add a warning 
+  cell_size <- round((n_pixels*xyres), 3)
+  cell_grid <- st_make_grid(rast.stack, cellsize = cell_size)
+  patch_cells <- lengths(st_within(cell_grid, 
+                                   st_as_sfc(st_bbox(rast.stack)))) >0
+  patch_grid <- cell_grid[patch_cells] #keep only the patches with complete data
+  
+  dir.create(paste0(output_patches_dir, '/', 'patched_images'))
+  image.patch.dir <- paste0(output_patches_dir, '/', 'patched_images')
+  
+  image_tiles <- for(i in 1:length(patch_grid)) {
+    tile_i <- rast.stack[[-1]] %>% 
+      crop(patch_grid[[i]])%>%
+      writeRaster(paste0(image.patch.dir,
+                         '/', site_name, '_image_patch_', i, '.tif'))
+  }
+  
+  dir.create(paste0(output_patches_dir, '/', 'patched_labs'))
+  label.patch.dir <- paste0(output_patches_dir, '/', 'patched_labs')
+  
+  label_tiles <- for(i in 1:length(patch_grid)) {
+    label_tile_i <- rast.stack[[1]] %>%
+      crop(patch_grid[[i]]) %>%
+      writeRaster(paste0(label.patch.dir,
+                         '/', site_name,'_label_patch_', i, '.tif'))
+  }
+  stop.time <- Sys.time()
+  overall_time <- stop.time - start.time
+  
+  print(paste('Elapsed time:', overall_time))
+  print(paste('Image and label tiles saved to:', output_patches_dir,
+              'Total # Label Patches:', length(label_tiles),
+              'Total # Image Patches', length(image_tiles)))
+  print(paste('Size of tiles:', cell_size))
+  return(c(overall_time, cell_size, image_tiles, label_tiles))
+}
+
+Slow_parallel_dl_training_tile <- function(image_raster, label_raster, n_pixels, output_path,
                             site_name, n_cores) {
   start.time <- Sys.time()
   cropped.labels <- crop(label_raster, image_raster)
@@ -141,7 +132,11 @@ dl_training_tile <- function(image_raster, label_raster, n_pixels, output_path,
               'Total # Label Patches:', length(label_tiles),
               'Total # Image Patches', length(image_tiles)))
   print(paste('Size of tiles:', cell_size))
+  return(c(overall_time, cell_size, image_tiles, label_tiles))
 }
+
+
+
 
 ########################################################
 image_path <- 'C:\\Users\\byoungberg\\OneDrive - SRUC\\Documents\\Files\\MicaClip.tif'
@@ -151,12 +146,13 @@ output_patches_dir <- 'C:\\Users\\byoungberg\\OneDrive - SRUC\\Documents\\Files'
 rasto <- rast(image_path, lyrs =rast_bands)
 labs <- vect(labels_vect_path)
 rasterise <- rasterize_labels(labs, field= 'Num_class', rasto)
+tiles <-dl_training_tile(rasto, rasterise[[2]], 64, output_patches_dir, 's1')
+
+
+
 
 t.crop <- crop(rast.stack,patch_grid)
 plot(t.crop$Num_class)
-
-test <- dl_training_tile(rasto, rasterise, 400, output_patches_dir, 's1', n_cores=7)
-
 
 rast.stack <- c(rasterise, rasto)
 n_pixels <-32
@@ -185,13 +181,8 @@ dir.create(paste0(output_patches_dir, '/', 'patched_labs'))
 lab.patch.dir <- paste0(output_patches_dir, '/', 'patched_labs')
 writeRaster(label_tile_i, paste0(lab.patch.dir,
                                  '/','label_patch_', i, '.tif'))
-}
-
-length(labs)
-patch_grid[[7]]
-
-test <- dl_training_tile(rasto, rasterise, 32, output_patches_dir, 's1', n_cores=7)
-
+test <- dl_training_tile(rasto, rasterise[[2]], 400, output_patches_dir, 's1', n_cores=7)
+test2 <- NOPARA_dl_training_tile(rasto, rasterise[[2]], 400, output_patches_dir, 's1')
 #Star Functions ----
 rasterize_labels <- function (labels_path, image_path, output_path) {
   labs<- read_sf(labels_path)
@@ -232,3 +223,101 @@ if exists(crop) && crop == FALSE {
 }
 
                    
+####terra functions test runs ####
+labs <- rasterize_labels(labels_vect_path, image_path, Field='Num_class')
+
+xyres <- res(rast)
+round(xyres[1], 3) == round(xyres[2], 3) #is it square?
+n_pixels <- 32 #number of pixels per tile#
+cell_size <- round((n_pixels*xyres), 3) #size (m) per patch
+cell_grid <- st_make_grid(rast, cellsize = cell_size)
+patch_cells <- lengths(st_within(cell_grid, st_as_sfc(st_bbox(rast)))) >0
+patch_grid <- cell_grid[patch_cells]
+plot(rast$MicaClip_1)
+plot(patch_grid, add = TRUE)
+rast <- c(labs, rast)
+
+for (i in 1:length(patch_grid)) { #add option for parallell and put in function
+  tile_i <- rast[[-'labels']] %>% 
+    crop(patch_grid[i])
+  dir.create(paste0(output_patches_dir, '/', 'patched_images'))
+  image.patch.dir <- paste0(output_patches_dir, '/', 'patched_images')
+  writeRaster(tile_i, paste0(image.patch.dir, '/','image_patch_', i, '.tif'))
+  
+  label_tile_i <- rast[['labels']] %>%
+    crop(patch_grid[i])
+  dir.create(paste0(output_patches_dir, '/', 'patched_labs'))
+  writeRaster(label_tile_i, paste0(label.patch.dir,
+                                   '/','label_patch_', i, '.tif'))
+}
+
+label_tile_I <- rast['Num_class'] %>% #[must be title or layer, make ground mask always 1]
+  crop(patch_grid[50])
+
+plot(label_tile_I)
+
+plot(crop(rast[[1]],patch_grid[1]))
+plot(patch_grid[1], add = TRUE)
+plot(patch_grid[c(1,2,3,4,5)])
+
+
+
+
+
+
+
+
+
+
+
+
+NOPARA_dl_training_tile <- function(image_raster, label_raster, n_pixels, output_path,
+                             site_name) {
+  start.time <- Sys.time()
+  cropped.labels <- crop(label_raster, image_raster)
+  rast.stack <- c(cropped.labels, image_raster)
+  xyres <- res(rast.stack)
+  round(xyres[1], 3) == round(xyres[2], 3) #is it square? ~~~~add a warning 
+  cell_size <- round((n_pixels*xyres), 3)
+  cell_grid <- st_make_grid(rast.stack, cellsize = cell_size)
+  patch_cells <- lengths(st_within(cell_grid, 
+                                   st_as_sfc(st_bbox(rast.stack)))) >0
+  patch_grid <- cell_grid[patch_cells] #keep only the patches with complete data
+
+  #if (missing(n_cores)) {
+   # n_cores <- detectCores()-4
+#  }
+ # cl <- makeCluster(n_cores) # we start the parallel cluster
+  #registerDoParallel(cl) # we register it
+  
+  dir.create(paste0(output_patches_dir, '/', 'patched_images'))
+  image.patch.dir <- paste0(output_patches_dir, '/', 'patched_images')
+  
+  image_tiles <- for(i in 1:length(patch_grid)) {
+                           tile_i <- rast.stack[[-1]] %>% 
+                             crop(patch_grid[[i]])%>%
+                             writeRaster(paste0(image.patch.dir,
+                                                '/', site_name, '_image_patch_', i, '.tif'))
+                         }
+  
+  
+  dir.create(paste0(output_patches_dir, '/', 'patched_labs'))
+  label.patch.dir <- paste0(output_patches_dir, '/', 'patched_labs')
+  
+  label_tiles <- for(i in 1:length(patch_grid)) {
+                           label_tile_i <- rast.stack[[1]] %>%
+                             crop(patch_grid[[i]]) %>%
+                             writeRaster(paste0(label.patch.dir,
+                                                '/', site_name,'_label_patch_', i, '.tif'))
+                         }
+  #stopCluster(cl)
+  stop.time <- Sys.time()
+  overall_time <- stop.time - start.time
+  
+  print(paste('Elapsed time:', overall_time))
+  print(paste('Image and label tiles saved to:', output_patches_dir,
+              'Total # Label Patches:', length(label_tiles),
+              'Total # Image Patches', length(image_tiles)))
+  print(paste('Size of tiles:', cell_size))
+  return(c(overall_time, cell_size, image_tiles, label_tiles))
+}
