c396ae7a8c47dedae05d67fb520274819be9f17f
bjyberg <bjyberg1@gmail.com>
2023-01-30 12:27:11 +0000

    Added Py models script

---
+++ Models.py
@@ -0,0 +1,197 @@
+from keras.models import Model
+from keras.models import Input, Conv2D, MaxPooling2D, UpSampling2D, Conv2DTranspose, Dropout, concatenate, ReLU
+
+
+def multi_unet_model(n_classes, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS):
+#Build the model
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    s = inputs
+
+    #Contraction path
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
+    c1 = Dropout(0.1)(c1)
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+    
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = Dropout(0.1)(c2)
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+     
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = Dropout(0.2)(c3)
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+     
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = Dropout(0.2)(c4)
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+     
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Dropout(0.3)(c5)
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    
+    #Expansive path 
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = Dropout(0.2)(c6)
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+     
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = Dropout(0.2)(c7)
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+     
+    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = Dropout(0.1)(c8)
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+     
+    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = Dropout(0.1)(c9)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+     
+    outputs = Conv2D(n_classes, (1, 1), activation='softmax')(c9)
+     
+    model = Model(inputs=[inputs], outputs=[outputs])
+    return model
+
+#####Keras model
+
+def get_keras_model(n_classes, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS):
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+
+    ### [First half of the network: downsampling inputs] ###
+
+    # Entry block
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    # Blocks 1, 2, 3 are identical apart from the feature depth.
+    for filters in [64, 128, 256]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(filters, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    ### [Second half of the network: upsampling inputs] ###
+
+    for filters in [256, 128, 64, 32]:
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.UpSampling2D(2)(x)
+
+        # Project residual
+        residual = layers.UpSampling2D(2)(previous_block_activation)
+        residual = layers.Conv2D(filters, 1, padding="same")(residual)
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    # Add a per-pixel classification layer
+    outputs = layers.Conv2D(num_classes, 3, activation="softmax", padding="same")(x)
+
+    # Define the model
+    model = keras.Model(inputs, outputs)
+    return model
+
+####Trippier Model 
+
+def trippier_model(n_classes, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS, kernel_init = 'he_normal'):
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+
+    conv1 = Conv2D(32, (3, 3), padding='same', kernel_initializer=kernel_init)(inputs)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = ReLU()(conv1)
+    conv1 = Conv2D(32, (3, 3), padding='same', kernel_initializer=kernel_init)(conv1)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = ReLU()(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), padding='same', kernel_initializer=kernel_init)(pool1)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = ReLU()(conv2)
+    conv2 = Conv2D(64, (3, 3), padding='same', kernel_initializer=kernel_init)(conv2)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = ReLU()(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), padding='same', kernel_initializer=kernel_init)(pool2)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = ReLU()(conv3)
+    conv3 = Conv2D(128, (3, 3), padding='same', kernel_initializer=kernel_init)(conv3)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = ReLU()(conv3)
+    up3 = concatenate([conv2, Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same',
+                                            kernel_initializer=kernel_init)(conv3)], axis=3)
+
+    conv4 = Conv2D(64, (3, 3), padding='same', kernel_initializer=kernel_init)(up3)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = ReLU()(conv4)
+    conv4 = Conv2D(64, (3, 3), padding='same', kernel_initializer=kernel_init)(conv4)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = ReLU()(conv4)
+    up4 = concatenate([conv1, Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same',
+                                            kernel_initializer=kernel_init)(conv4)], axis=3)
+
+    conv5 = Conv2D(32, (3, 3), padding='same', kernel_initializer=kernel_init)(up4)
+    conv5 = BatchNormalization()(conv5)
+    conv5 = ReLU()(conv5)
+    conv5 = Conv2D(32, (3, 3), padding='same', kernel_initializer=kernel_init)(conv5)
+    conv5 = BatchNormalization()(conv5)
+    conv5 = ReLU()(conv5)
+
+    conv6 = Conv2D(n_classes, (1, 1), activation='sigmoid')(conv5)
+
+    # model for training with image-level labels
+    trippier_model = keras.models.Model(inputs, conv6)
+
+#### Simple FCN 
+def simple_FCNN(N_class, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS):
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    c1 = Conv2D(filters=16, kernel_size=3, strides=2, activation="relu", padding="same")(inputs)
+    c1 = ReLU()(c1)
+    c2 = Conv2D(filters=32, kernel_size=3, strides=2, activation="relu", padding="same")(c1)
+    c2 = ReLU()(c2)
+    c3 = Conv2D(filters=64, kernel_size=3, strides=2, activation="relu", padding="same")(c2)
+    c3 = ReLU()(c3)
+    c4 = Conv2D(filters=64, kernel_size=3, strides=2, activation="relu", padding="same")(c3)
+    C4 = ReLU()(c4)
+    t1 = Conv2DTranspose(filters=64, kernel_size=3, strides=2, activation='relu', padding="same")(c4)
+    t1 = ReLU()(t1)
+    t2 = Conv2DTranspose(filters=32, kernel_size=3, strides=2, activation='relu', padding="same")(t1)
+    t2 = ReLU()(t2)
+    t3 = Conv2DTranspose(filters=16, kernel_size=3, strides=2, activation='relu', padding="same")(t2)
+    t3 = ReLU()(t3)
+    t4 = Conv2DTranspose(filters=N_class, kernel_size=3, strides=2, activation='relu', padding="same")(t3)
+    outputs = Conv2D(N_class, (1, 1), activation='softmax')(t4) #or sigmoid/softmax
+     
+    model = Model(inputs=[inputs], outputs=[outputs])
+    return model
+
